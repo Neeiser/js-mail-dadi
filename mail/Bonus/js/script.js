99c1c82a9209c2815aa34af2a70900961d217213
Neeiser <stortini.study@gmail.com>
2022-02-24 18:14:31 +0100

    Bonus: Bug

--- mail/Bonus/js/script.js
+++ mail/Bonus/js/script.js
@@ -1,25 +1,26 @@
 
-const mailArrey = ['polpo@gmail.com', 'zebra@gmail.com', 'gatto@gmail.com', 'cane@gmail.com', 'caimano@gmail.com', 'anatra@gmail.com', 'gorilla@gmail.com', 'tartaruga@gmail.com', 'pulcino@gmail.com', 'pinguino@gmail.com', ];
 
 let userWroteMail = document.getElementById('user-mail');
 let userSubmittedMail = document.getElementById('user-mail-submit');
 let userMailResult = document.getElementById('mail-test-result');
 
+const mailArrey = ['polpo@gmail.com', 'zebra@gmail.com', 'gatto@gmail.com', 'cane@gmail.com', 'caimano@gmail.com', 'anatra@gmail.com', 'gorilla@gmail.com', 'tartaruga@gmail.com', 'pulcino@gmail.com', 'pinguino@gmail.com', ];
 let match= false;
 
 userSubmittedMail.addEventListener("click", function() {
     
     for (let mailCount = 0; mailCount < mailArrey.length; mailCount++){
-    
+
         if (mailArrey[mailCount] == userWroteMail) {
             match= true;
         }
-    };
+    console.log(match); /* Non cambia in TRUE! */
+    }
 
     if (match == true) {
         userMailResult.innerHTML = ('You are the chosen one!');
-        console.log(match);
     } else {
         userMailResult.innerHTML = ('Go away!');
-    }  
+    }
 });
+
