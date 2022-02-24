c115a9fdb0cbf53efd1dffb06794c44e89b6d602
Neeiser <stortini.study@gmail.com>
2022-02-24 15:33:22 +0100

    Mail exercise: Loop created

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,3 +1,21 @@
 /* 1: Chiedi all’utente la sua email */
 /* 2: Controlla che sia nella lista di chi può accedere. */
 /* 3: Stampa un messaggio appropriato sull’esito del controllo. */
+
+
+/* 1 */
+let userMail = prompt('Inserisci la tua mail');
+
+
+/* 2 */
+const mailArrey = ['polpo@gmail.com', 'zebra@gmail.com', 'gatto@gmail.com', 'cane@gmail.com', 'caimano@gmail.com', 'anatra@gmail.com', 'gorilla@gmail.com', 'tartaruga@gmail.com', 'pulcino@gmail.com', 'pinguino@gmail.com', ];
+
+for (let i = 0; i < mailArrey.length; i++){
+
+    if (mailArrey[i] == userMail) {
+        console.log('registrato')
+    } else {
+        console.log('non registrato')
+    }
+
+}
