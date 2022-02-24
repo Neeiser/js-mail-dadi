6fca3ab43ec00d0dd34fddfdc32b0d80e75b50c7
Neeiser <stortini.study@gmail.com>
2022-02-24 17:11:25 +0100

    Dice exercise: Result Visualization

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -7,16 +7,21 @@
 let randomUserNumber = Math.floor(Math.random() * 10) +1;   /* Numero intero random da 1 a 10 */
 let randomComputerNumber = Math.floor(Math.random() * 10) +1;   /* Numero intero random da 1 a 10 */
 
-console.log('Il tuo tiro è di: ' + randomUserNumber);
-console.log('Il tiro del computer è di: ' + randomComputerNumber);
+/* BONUS: */
+
+let UserNumberBox = document.getElementById('user-number');
+let ComputerNumberBox = document.getElementById('computer-number');
+let winnerBox = document.getElementById('winner');
+UserNumberBox.innerHTML = ('Il tuo tiro è di: ' + randomUserNumber);
+ComputerNumberBox.innerHTML = ('Il tiro del computer è di: ' + randomComputerNumber);
 
 
 /* 2 */
 
 if (randomUserNumber > randomComputerNumber) {
-    console.log('Hai vinto!');
+    winnerBox.innerHTML = ('Hai vinto!');
 } else if (randomUserNumber < randomComputerNumber) {
-    console.log('Hai perso!');
+    winnerBox.innerHTML = ('Hai perso!');
 } else {
-    console.log('Pareggio!')
+    winnerBox.innerHTML = ('Pareggio!')
 }
