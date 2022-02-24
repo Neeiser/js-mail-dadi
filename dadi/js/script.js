8fbceef30bcb9be540e4448176e6534bd4925038
Neeiser <stortini.study@gmail.com>
2022-02-24 16:01:51 +0100

    Dice exercise done

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -7,4 +7,16 @@
 let randomUserNumber = Math.floor(Math.random() * 10) +1;   /* Numero intero random da 1 a 10 */
 let randomComputerNumber = Math.floor(Math.random() * 10) +1;   /* Numero intero random da 1 a 10 */
 
-console.log()
+console.log('Il tuo tiro è di: ' + randomUserNumber);
+console.log('Il tiro del computer è di: ' + randomComputerNumber);
+
+
+/* 2 */
+
+if (randomUserNumber > randomComputerNumber) {
+    console.log('Hai vinto!');
+} else if (randomUserNumber < randomComputerNumber) {
+    console.log('Hai perso!');
+} else {
+    console.log('Pareggio!')
+}
