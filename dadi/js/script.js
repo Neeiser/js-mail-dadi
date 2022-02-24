ad7268bf5fb83c5e43998083c60a73c050835b69
Neeiser <stortini.study@gmail.com>
2022-02-24 15:52:48 +0100

    Dice: Random numbers generator

---
+++ dadi/js/script.js
@@ -0,0 +1,10 @@
+/* 1: Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. */
+/* 2: Stabilire il vincitore, in base a chi fa il punteggio più alto. */
+
+
+/* 1 */
+
+let randomUserNumber = Math.floor(Math.random() * 10) +1;   /* Numero intero random da 1 a 10 */
+let randomComputerNumber = Math.floor(Math.random() * 10) +1;   /* Numero intero random da 1 a 10 */
+
+console.log()
