b971b725525935913145b7e423ac25f0c93a68d3
Neeiser <stortini.study@gmail.com>
2022-02-25 11:30:09 +0100

    Bug Fixed (finally)

--- mail/Bonus/js/script.js
+++ mail/Bonus/js/script.js
@@ -1,20 +1,19 @@
 
-
-let userWroteMail = document.getElementById('user-mail');
-let userSubmittedMail = document.getElementById('user-mail-submit');
-let userMailResult = document.getElementById('mail-test-result');
+const userSubmittedMail = document.getElementById('user-mail-submit');
+const userMailResult = document.getElementById('mail-test-result');
 
 const mailArrey = ['polpo@gmail.com', 'zebra@gmail.com', 'gatto@gmail.com', 'cane@gmail.com', 'caimano@gmail.com', 'anatra@gmail.com', 'gorilla@gmail.com', 'tartaruga@gmail.com', 'pulcino@gmail.com', 'pinguino@gmail.com', ];
-let match= false;
 
 userSubmittedMail.addEventListener("click", function() {
+    const userWroteMail = document.getElementById('user-mail').value;
+    let match= false;
     
     for (let mailCount = 0; mailCount < mailArrey.length; mailCount++){
 
         if (mailArrey[mailCount] == userWroteMail) {
             match= true;
+            console.log(userWroteMail); /* NON LO TROVA  */
         }
-    console.log(match); /* Non cambia in TRUE! */
     }
 
     if (match == true) {
@@ -22,5 +21,6 @@ userSubmittedMail.addEventListener("click", function() {
     } else {
         userMailResult.innerHTML = ('Go away!');
     }
+    
 });
 
